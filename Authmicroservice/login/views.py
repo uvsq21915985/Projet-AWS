7fe17dbb6ac95c07f29e03f6a1f3725eb6f60987
uvsq22404690 <riadnamane@gmail.com>
2025-03-19 23:09:07 +0100

    overriding the permission classes to allow any for APIs that don't require authentication and fixed logout API by expiring the tokens

--- Authmicroservice/login/views.py
+++ Authmicroservice/login/views.py
@@ -1,24 +1,21 @@
 from rest_framework import status
 from .serializers import UserSerializer
-#from rest_framework.authtoken.models import Token
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from django.core.exceptions import ObjectDoesNotExist
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
-#from rest_framework.authentication import TokenAuthentication
 from django.conf import settings
 from .models import CustomUser
 from .authentification import CustomJWTAuthentication
-from django.views.decorators.csrf import csrf_exempt
 
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 @authentication_classes([])
-@permission_classes([]) 
 def register_user(request):
     if request.method == 'POST':
         serializer = UserSerializer(data=request.data)
@@ -29,11 +26,10 @@ def register_user(request):
     
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 @authentication_classes([])
-@permission_classes([]) 
 def user_login(request):
     if request.method == 'POST':
-        print("in user login")
         username = request.data.get('username')
         password = request.data.get('password')
         if not username:
@@ -49,20 +45,11 @@ def user_login(request):
             except ObjectDoesNotExist:
                 pass
         if user:
-            print("user log in")
-            print(user)
-            #token, _ = Token.objects.get_or_create(user=user)
-            #print(token)
-            #return Response({'token': token.key}, status=status.HTTP_200_OK)
-           # token = CustomTokenObtainPairView.as_view()(request._request)
-           # refresh = token.data.get('refresh')
-            #access = token.data.get('access')
             refresh = RefreshToken.for_user(user)
             access_token = refresh.access_token
             refresh_token= str(refresh)
             response = Response({
             'message': 'Login successful'
-#            ,'data': {'username': user.username}, 'token': {str(access_token)}
         }) 
             response.set_cookie(
                                     key = settings.SIMPLE_JWT['AUTH_COOKIE'], 
@@ -80,32 +67,23 @@ def user_login(request):
                                     httponly = settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
                                     samesite = settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE']
             )
-            #csrf.get_token(request)
-            print("sucessful")
-         #   response.data = {"Success" : "Login successfully","data": {"username": user.username}}
-
             return response
         else:
             return Response({'error': 'invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
     else:
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
+
 @api_view(['GET'])
 @authentication_classes([CustomJWTAuthentication]) 
 @permission_classes([IsAuthenticated])
 def validate_token(request):
-    print("IN VALIDATE")
-    print("Cookies FOUND IN VALIDATE:", request.COOKIES)
     token =  request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE'])
-    print(f"VALIDATE AUTH TOKEN : {token}")
-    print(f"request user {request.user}")
     if not token:
         return Response({'error': 'No token found in cookies'}, status=status.HTTP_401_UNAUTHORIZED)
     try:
         user_id = request.user.id
         print(f"USER ID{user_id}")
-       # user_id = validated_token['user_id']  # Access the user_id from the token
-        # get user from database
         user = CustomUser.objects.get(id=user_id) 
 
     except Exception as e:
@@ -116,45 +94,46 @@ def validate_token(request):
 
 
 @api_view(['POST'])
-@authentication_classes([])
-@permission_classes([])
+@permission_classes([IsAuthenticatedOrReadOnly])  # ensure only logged-in users can log out
 def user_logout(request):
-    if request.method == 'POST':
-        try:
-            print("logout")
-            refresh = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
-            print(f"token found: {refresh}")
-            #access = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE'])
-            if refresh:
-                print("refresh exist")
-                token_r = RefreshToken(refresh)
-                print("TOKEN R {token_r}")
-                #token_r.blacklist()
-                response = Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
-                response.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE'])
-                response.delete_cookie(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
-                
-                return response
-            else:
-                return Response({'error': 'No token found in cookies'}, status=status.HTTP_401_UNAUTHORIZED)
-        except Exception as e:
-            print(f"Error during logout: {str(e)}")
-            return Response(status=status.HTTP_400_BAD_REQUEST)
-        #try:
-            # Delete the user's token to logout
-         #   request.user.auth_token.delete()
-          #  return Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
-        #except Exception as e:
-         #   return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    else:
-        return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
+    try:
+        refresh = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
+
+        if not refresh:
+            return Response({'error': 'No refresh token found in cookies'}, status=status.HTTP_401_UNAUTHORIZED)
+
+        response = Response({'message': 'Successfully logged out.'}, status=status.HTTP_200_OK)
+
+        # expire the cookies on the server-side
+        response.set_cookie(
+            settings.SIMPLE_JWT['AUTH_COOKIE'],
+            '',
+            expires='Thu, 01 Jan 1970 00:00:00 GMT',
+            secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
+            httponly=settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+            samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE'],
+        )
+        response.set_cookie(
+            settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'],
+            '',
+            expires='Thu, 01 Jan 1970 00:00:00 GMT',
+            secure=settings.SIMPLE_JWT['AUTH_COOKIE_SECURE'],
+            httponly=settings.SIMPLE_JWT['AUTH_COOKIE_HTTP_ONLY'],
+            samesite=settings.SIMPLE_JWT['AUTH_COOKIE_SAMESITE'],
+        )
+
+        return response
+
+    except Exception as e:
+        print(f"Error during logout: {str(e)}")
+        return Response({'error': 'Something went wrong'}, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['POST'])
 @authentication_classes([])  
-@permission_classes([])
+@permission_classes([IsAuthenticatedOrReadOnly])
 def refresh_token(request):
     if request.method == 'POST':
-      #  print(f"USER : {request.user}")
         refresh_token = request.COOKIES.get(settings.SIMPLE_JWT['AUTH_COOKIE_REFRESH'])
         if not refresh_token:
             return Response({'error': 'refresh token not found in cookies'}, status=status.HTTP_400_BAD_REQUEST)
