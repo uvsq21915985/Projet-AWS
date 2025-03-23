d77b2c34b95eb40e2a30acb51fab984525e829a6
uvsq22404690 <riadnamane@gmail.com>
2025-03-23 03:58:45 +0100

    temp fix

--- frontend/src/app/(layout-video)/videoConference/page.tsx
+++ frontend/src/app/(layout-video)/videoConference/page.tsx
@@ -1,70 +1,70 @@
-// "use client";
+"use client";
 
 // import { useEffect, useState } from 'react';
 // import { useRouter, useSearchParams } from 'next/navigation';
-// import Jitsit from '@/components/Jitsi/Jitsi';
+import Jitsit from '@/components/Jitsi/Jitsi';
 // import ErrorPage from '@/components/ErrorPage/ErrorPage';
-// import {create_room } from "@/services/auth";
+import {create_room } from "@/services/auth";
 // import { Suspense } from 'react'
-// import Loading from '@/components/Loading/Loading';
-// import LocalStorage from '@/app/hooks/LocalStorage';
+import Loading from '@/components/Loading/Loading';
+import LocalStorage from '@/app/hooks/LocalStorage';
 
-// /*
-// page for creating a meeting
+/*
+page for creating a meeting
 
-// */ 
-// export default function VideoConference() {
-//   const router = useRouter(); // handle change of url
-//   const searchParams = useSearchParams();
-//   const [roomId, setRoomId] = useState('');
-//   const [subject, setSubject] = useState('');
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(false);
+*/ 
+export default function VideoConference() {
+  // const router = useRouter(); // handle change of url
+  // const searchParams = useSearchParams();
+  // const [roomId, setRoomId] = useState('');
+  // const [subject, setSubject] = useState('');
+  // const [loading, setLoading] = useState(true);
+  // const [error, setError] = useState(false);
 
-//   const creatingRoom = async () => {
-//     try{
-//     setLoading(true);
-//     const subject = searchParams.get('subject');
-//     let roomID= await create_room(Date.now());
-//     if (roomID && subject){   
-//       setSubject(subject);
-//       setRoomId(roomID);
-//       setLoading(false);
-//       setLoading(false);   
-//     }else{setError(true);}
-//     }catch(e){
-//       setError(true);
-//     }
-//   }
+  // const creatingRoom = async () => {
+  //   try{
+  //   setLoading(true);
+  //   const subject = searchParams.get('subject');
+  //   let roomID= await create_room(Date.now());
+  //   if (roomID && subject){   
+  //     setSubject(subject);
+  //     setRoomId(roomID);
+  //     setLoading(false);
+  //     setLoading(false);   
+  //   }else{setError(true);}
+  //   }catch(e){
+  //     setError(true);
+  //   }
+  // }
   
-//   useEffect(()=>{
-//     creatingRoom();
+  // useEffect(()=>{
+  //   creatingRoom();
    
-//   },[])
+  // },[])
 
 
 
 
-// return (
-// <div>
-//   {error ? <ErrorPage></ErrorPage>:
-//     loading ? <Loading /> : <Jitsit id={roomId} subject={subject} />
-//   }
-// </div>
+return (
+<div>
+  {error ? <ErrorPage></ErrorPage>:
+    loading ? <Loading /> : <Jitsit id={roomId} subject={subject} />
+  }
+</div>
 
-// );
-
-
-// }
+);
 
 
+}
 
 
-// /*
-//  { error &&  <p>Erreur lors du chargement veuillez réessayez</p> } 
-//   { !error && !loading && <Jitsit id={roomId} ></Jitsit> }
-//   {!error && loading && <div className='call empty-list'><p>Chargement...</p></div>} 
 
-// */
+
+/*
+ { error &&  <p>Erreur lors du chargement veuillez réessayez</p> } 
+  { !error && !loading && <Jitsit id={roomId} ></Jitsit> }
+  {!error && loading && <div className='call empty-list'><p>Chargement...</p></div>} 
+
+*/
 
 
